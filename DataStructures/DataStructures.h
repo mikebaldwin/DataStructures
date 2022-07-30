c94435c04db4e9b76b0b74fe1c1b21073b2a594c
Mike Baldwin <mike.baldwin@willowtreeapps.com>
2022-07-30 13:53:41 -0400

    Initial Commit

---
+++ DataStructures/DataStructures.h
@@ -0,0 +1,18 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Mike Baldwin on 7/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
